913654971dc6bd3de9d2b991dd981347f9d076b4
Shardul Singh <shardulsinghtomar@gmail.com>
2021-06-05 21:56:33 +0530

    changed face

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ function init()
 	canvas = document.getElementById('mycanvas');
 	console.log(canvas);
 	W=canvas.width=1400;
-	H=canvas.height=1300;
+	H=canvas.height=1250;
 	pen = canvas.getContext('2d');
 	console.log(pen);
 	soldier={
@@ -21,7 +21,7 @@ function init()
 
 	box1={
 		x:250,
-		y:800,
+		y:900,
 		w:60,
 		h:60,
 		speed:1
@@ -29,10 +29,10 @@ function init()
 
 	box2={
 		x:450,
-		y:0,
+		y:800,
 		w:60,
 		h:60,
-		speed:1
+		speed:-1
 	};
 
 
@@ -60,7 +60,7 @@ function init()
 	};
 
 	soldier_img=new Image();
-	soldier_img.src="assets/img_face.jpeg";
+	soldier_img.src="assets/img_face.png";
 
 	virus_img1=new Image();
 	virus_img1.src="assets/img_virus1.png";
@@ -85,7 +85,7 @@ function init()
 	}
 	classes=[box1,box2,box3,box4,soldier,gem];
 	imgs=[virus_img1,virus_img2,virus_img3,virus_img3,soldier_img,gem_img];
-	sz=[2,2,2,2,0.8,3];
+	sz=[2,2,2,2,1,3];
 	canvas.addEventListener('mousedown',keyPressed);
 	canvas.addEventListener('mouseup',keyPressed1);
 
@@ -94,6 +94,7 @@ function init()
 function draw()
 {
 	pen.clearRect(0,0,W,H);
+	pen.fillText("!",50,500);
 	pen.fillStyle="red";
 	for(let i=0;i<imgs.length;i++)
 		pen.drawImage(imgs[i],classes[i].x,classes[i].y,classes[i].w*sz[i],classes[i].h*sz[i]);
@@ -108,30 +109,46 @@ function update()
 		pen.fillText("YOU WON!!",450,650);
 		clearInterval(f);
 	}
-
+	/*(((box.x<soldier.x && soldier.x<(box.x+box.w))&&(box.y<soldier.y && soldier.y<(box.y+box.w ))) || 
+		((box.x<soldier.x && soldier.x<(box.x+box.w))&&(soldier.y<box.y && box.y<(soldier.y+soldier.w))) ||
+		((soldier.x<box.x && box.x<(soldier.x+soldier.w))&&((box.y<soldier.y && soldier.y<(box.y+box.w )))) ||
+		((soldier.x<box.x && box.x<(soldier.x+soldier.w))&&(box.y<soldier.y && soldier.y<(box.y+box.w ))))*/
+	
 	console.log(soldier.x);
-	if(soldier.x>=box1.x && soldier.x<=box1.x+120 && soldier.y>=box1.y &&soldier.y<=box1.y+120)
+	if(((box1.x<soldier.x && soldier.x<(box1.x+2*box1.w))&&(box1.y<soldier.y && soldier.y<(box1.y+2*box1.w ))) || 
+		((box1.x<soldier.x && soldier.x<(box1.x+2*box1.w))&&(soldier.y<box1.y && box1.y<(soldier.y+soldier.h))) ||
+		((soldier.x<box1.x && box1.x<(soldier.x+soldier.w))&&((box1.y<soldier.y && soldier.y<(box1.y+2*box1.w )))) ||
+		((soldier.x<box1.x && box1.x<(soldier.x+soldier.w))&&(soldier.y<box1.y && box1.y<(soldier.y+soldier.h))))
 		{
 			clearInterval(f);
 			pen.fillStyle="Red";
 			pen.font="100px roboto";
 			pen.fillText("GAME OVER!",400,650);
 		}
-	else if(soldier.x>=box2.x && soldier.x<=box2.x+120 && soldier.y>=box2.y &&soldier.y<=box2.y+120)
+	else if(((box2.x<soldier.x && soldier.x<(box2.x+2*box2.w))&&(box2.y<soldier.y && soldier.y<(box2.y+2*box2.w ))) || 
+		((box2.x<soldier.x && soldier.x<(box2.x+2*box2.w))&&(soldier.y<box2.y && box2.y<(soldier.y+soldier.h))) ||
+		((soldier.x<box2.x && box2.x<(soldier.x+soldier.w))&&((box2.y<soldier.y && soldier.y<(box2.y+2*box2.w )))) ||
+		((soldier.x<box2.x && box2.x<(soldier.x+soldier.w))&&(soldier.y<box2.y && box2.y<(soldier.y+soldier.h))))
 		{
 			clearInterval(f);
 			pen.fillStyle="Red";
 			pen.font="100px roboto";
 			pen.fillText("GAME OVER!",400,650);
 		}
-	else if(soldier.x>=box3.x && soldier.x<=box3.x+120 && soldier.y>=box3.y &&soldier.y<=box3.y+120)
+	else if(((box3.x<soldier.x && soldier.x<(box3.x+2*box3.w))&&(box3.y<soldier.y && soldier.y<(box3.y+2*box3.w ))) || 
+		((box3.x<soldier.x && soldier.x<(box3.x+2*box3.w))&&(soldier.y<box3.y && box3.y<(soldier.y+soldier.h))) ||
+		((soldier.x<box3.x && box3.x<(soldier.x+soldier.w))&&((box3.y<soldier.y && soldier.y<(box3.y+2*box3.w )))) ||
+		((soldier.x<box3.x && box3.x<(soldier.x+soldier.w))&&(soldier.y<box3.y && box3.y<(soldier.y+soldier.h))))
 		{
 			clearInterval(f);
 			pen.fillStyle="Red";
 			pen.font="100px roboto";
 			pen.fillText("GAME OVER!",400,650);
 		}
-	else if(soldier.x>=box4.x && soldier.x<=box4.x+120 && soldier.y>=box4.y &&soldier.y<=box4.y+120)
+	else if(((box4.x<soldier.x && soldier.x<(box4.x+2*box4.w))&&(box4.y<soldier.y && soldier.y<(box4.y+2*box4.w ))) || 
+		((box4.x<soldier.x && soldier.x<(box4.x+2*box4.w))&&(soldier.y<box4.y && box4.y<(soldier.y+soldier.h))) ||
+		((soldier.x<box4.x && box4.x<(soldier.x+soldier.w))&&((box4.y<soldier.y && soldier.y<(box4.y+2*box4.w )))) ||
+		((soldier.x<box4.x && box4.x<(soldier.x+soldier.w))&&(soldier.y<box4.y && box4.y<(soldier.y+soldier.h))))
 		{
 			clearInterval(f);
 			pen.fillStyle="Red";
